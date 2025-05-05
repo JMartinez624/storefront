7d3d1e1c77bf8b6c41e963f33799e60339b18300
Juan Martinez <juanmartinez@Mac.home.local>
2025-05-04 19:31:50 -0500

    try new

--- myScript.js
+++ myScript.js
@@ -1,177 +1,124 @@
 const products_in_cart = {};
 let total = 0;
-console.log(products_in_cart);
-let zero = true;
 
+function addToCart(product, price) {
+    const name = product.replace(/\s/g, '');
+    
+    if (!products_in_cart[product]) {
+        products_in_cart[product] = {
+            quantity: 1,
+            total: parseFloat(price)
+        };
+    } else {
+        products_in_cart[product].quantity += 1;
+        products_in_cart[product].total += parseFloat(price);
+    }
+
+    total += parseFloat(price);
+    updateCartUI(product, price);
+    updateButtonUI(product, price);
+    updateCartSummary();
+}
 
 function increaseItem(product, price) {
-    var name = product.replace(/\s/g, '');
-    let flag = false;
+    products_in_cart[product].quantity += 1;
+    products_in_cart[product].total += parseFloat(price);
+    total += parseFloat(price);
 
-    if (products_in_cart[product] == null){
-        products_in_cart[product]["quantity"] += 1;
-        products_in_cart[product]["total"] += parseFloat(price);
-        flag = true;
-
-
-        total += parseFloat(price);
-
-        var value = document.getElementById('total').innerText;
-        value++;
-        document.getElementById('total').innerText = value;
-    } else {
-        products_in_cart[product]["quantity"] += 1;
-        products_in_cart[product]["total"] += parseFloat(price);
-    }
-
-    document.getElementById(`add-${name}`).innerHTML = `
-        <div style="display: flex; justify-content: space-between; color: white;">
-            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-left: 5px;" src="./assets/images/icon-decrement-quantity.svg" onclick="decreaseItem('${product}', ${price})"/>
-            ${products_in_cart[product]["quantity"]}
-            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-right: 5px;" src="./assets/images/icon-increment-quantity.svg" onclick="increaseItem('${product}', ${price})"/>
-        </div>
-    `;
-
-
-    var value = document.getElementById('total').innerText;
-    value++;
-    document.getElementById('total').innerText = value;
-
-    if (flag) { 
-        document.getElementById('cart-items').innerHTML += `
-        <div id='${name}_cart' style="display: flex;">
-            <div style="display:flex, flex-direction: column; width: 400px; padding-right: 30px;">
-                <h3>
-                ${product}</h3> <h3> <span style="color: hsl(14, 86%, 42%)" id='${name}_quantity'>${products_in_cart[product].quantity}x</span>&ensp; <span style="color: hsl(14, 25%, 72%); padding-right: 5px;
-                ">@ $${price}</span> <span style="color: hsl(12, 20%, 44%)" id='${name}_total'>$${products_in_cart[product].total.toFixed(2)}</span>
-                </h3>
-            </div>
-            <div style="display: flex; align-items: center;">
-                <img id="tru" style="border: 2px solid hsl(14, 25%, 72%); border-radius: 25px; height: 15px; padding: 1px;" onclick="removeFromCart('${product}')" src="./assets/images/icon-remove-item.svg"/>
-            </div>
-        </div>
-        `;
-        document.getElementById('cart-total').innerText = total.toFixed(2);
-    } else {
-        document.querySelector(`#${name}_quantity`).innerText = `${products_in_cart[product]["quantity"]}x`;
-        document.querySelector(`#${name}_total`).innerText = `$${products_in_cart[product]["total"].toFixed(2)}`;
-        document.getElementById('cart-total').innerText = total.toFixed(2);
-    }
+    updateButtonUI(product, price);
+    updateCartUI(product, price);
+    updateCartSummary();
 }
 
 function decreaseItem(product, price) {
-    // console.log(products_in_cart);
-    var name = product.replace(/\s/g, '');
-    console.log(products_in_cart[product]);
+    if (!products_in_cart[product]) return;
 
-    if (products_in_cart[product] != null){
-        products_in_cart[product]["quantity"] -= 1;
-        products_in_cart[product]["total"] -= parseFloat(price);
-        document.getElementById(`add-${name}`).innerHTML = `
-        <div style="display: flex; justify-content: space-between; color: white;">
-            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-left: 5px;" src="./assets/images/icon-decrement-quantity.svg" onclick="decreaseItem('${product}', ${price})"/>
-            ${products_in_cart[product]["quantity"]}
-            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-right: 5px;" src="./assets/images/icon-increment-quantity.svg" onclick="increaseItem('${product}', ${price})"/>
-        </div>
-        `;
-    
-        total -= parseFloat(price);
-    
-        var value = document.getElementById('total').innerText;
-        value--;
-        document.getElementById('total').innerText = value; 
-        if (products_in_cart[product]["quantity"] == 0) {
-            console.log("heelo");
-            delete products_in_cart[product];
-            console.log(products_in_cart);
-        }
+    products_in_cart[product].quantity -= 1;
+    products_in_cart[product].total -= parseFloat(price);
+    total -= parseFloat(price);
+
+    if (products_in_cart[product].quantity <= 0) {
+        removeFromCart(product);
     } else {
-        console.log('here');
-        document.getElementById(`add-${name}`).innerHTML = `
-        <span onclick="addToCart('Waffle with Berries', '6.50')"><img src="./assets/images/icon-add-to-cart.svg"/> <span id="add-text">Add to Cart</span></span>
-        `;
+        updateButtonUI(product, price);
+        updateCartUI(product, price);
+        updateCartSummary();
     }
-    
-}
-
-function popUp () {
-    document.querySelector('.product-page').innerHTML += `
-        <dialog open>
-        Hello World
-        </dialog>
-        `;
 }
 
 function removeFromCart(product) {
-    const quantity_to_remove = products_in_cart[product]["quantity"];
-    document.getElementById('total').innerText -= quantity_to_remove;
+    const name = product.replace(/\s/g, '');
+    
+    if (!products_in_cart[product]) return;
 
-    const cost_to_remove = products_in_cart[product]["total"];
-    total = total.toFixed(2) - cost_to_remove.toFixed(2);
-    document.getElementById('cart-total').innerText = total.toFixed(2);
-
-    var name = product.replace(/\s/g, '');
+    total -= products_in_cart[product].total;
     delete products_in_cart[product];
-    const element = document.getElementById(`${name}_cart`);
-    element.remove();
+
+    const cartItem = document.getElementById(`${name}_cart`);
+    if (cartItem) cartItem.remove();
+
+    const totalItems = Object.values(products_in_cart).reduce((sum, item) => sum + item.quantity, 0);
+    document.getElementById('total').innerText = totalItems;
+
+    updateCartSummary();
+
+    // Reset button to Add to Cart
+    const button = document.getElementById(`add-${name}`);
+    button.style.backgroundColor = "";
+    button.innerHTML = `
+        <span onclick="addToCart('${product}', '${price}')">
+            <img src="./assets/images/icon-add-to-cart.svg"/>
+            <span id="add-text">Add to Cart</span>
+        </span>`;
 }
 
-function addToCart(product, price) {
-    var name = product.replace(/\s/g, '');
-    let flag = false;
-    // total += parseFloat(price);
+function updateButtonUI(product, price) {
+    const name = product.replace(/\s/g, '');
+    const qty = products_in_cart[product].quantity;
+    console.log(name);
 
-
-    if (products_in_cart[product] == null){
-        products_in_cart[product] = {};
-        products_in_cart[product]["quantity"] = 1;
-        products_in_cart[product]["total"] = parseFloat(price);
-        flag = true;
-
-        document.getElementById(`add-${name}`).style.backgroundColor = "hsl(14, 86%, 42%)";
-        document.getElementById(`add-${name}`).innerHTML = `
+    document.getElementById(`add-${name}`).style.backgroundColor = "hsl(14, 86%, 42%)";
+    document.getElementById(`add-${name}`).innerHTML = `
         <div style="display: flex; justify-content: space-between; color: white;">
-            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-left: 5px;" src="./assets/images/icon-decrement-quantity.svg" onclick="decreaseItem('${product}', ${price})"/>
-            ${products_in_cart[product]["quantity"]}
-            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-right: 5px;" src="./assets/images/icon-increment-quantity.svg" onclick="increaseItem('${product}', ${price})"/>
-        </div>
-        `;
+            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-left: 5px;"
+                src="./assets/images/icon-decrement-quantity.svg"
+                onclick="decreaseItem('${product}', ${price})"/>
+            ${qty}
+            <img style="border: 2px solid white; border-radius: 25px; width: 10px; padding: 2px; margin-right: 5px;"
+                src="./assets/images/icon-increment-quantity.svg"
+                onclick="increaseItem('${product}', ${price})"/>
+        </div>`;
+}
 
-        total += parseFloat(price);
+function updateCartUI(product, price) {
+    const name = product.replace(/\s/g, '');
+    const cartItem = document.getElementById(`${name}_cart`);
 
-        var value = document.getElementById('total').innerText;
-        value++;
-        document.getElementById('total').innerText = value;
-    } 
-    // else {
-    //     products_in_cart[product]["quantity"] += 1;
-    //     products_in_cart[product]["total"] += parseFloat(price);
-    // }
-
-
-    // var value = document.getElementById('total').innerText;
-    // value++;
-    // document.getElementById('total').innerText = value;
-
-    if (flag) { 
+    if (!cartItem) {
         document.getElementById('cart-items').innerHTML += `
         <div id='${name}_cart' style="display: flex;">
-            <div style="display:flex, flex-direction: column; width: 400px; padding-right: 30px;">
+            <div style="display: flex; flex-direction: column; width: 400px; padding-right: 30px;">
+                <h3>${product}</h3>
                 <h3>
-                ${product}</h3> <h3> <span style="color: hsl(14, 86%, 42%)" id='${name}_quantity'>${products_in_cart[product].quantity}x</span>&ensp; <span style="color: hsl(14, 25%, 72%); padding-right: 5px;
-                ">@ $${price}</span> <span style="color: hsl(12, 20%, 44%)" id='${name}_total'>$${products_in_cart[product].total.toFixed(2)}</span>
+                    <span style="color: hsl(14, 86%, 42%)" id='${name}_quantity'>${products_in_cart[product].quantity}x</span>&ensp;
+                    <span style="color: hsl(14, 25%, 72%); padding-right: 5px;">@ $${price}</span>
+                    <span style="color: hsl(12, 20%, 44%)" id='${name}_total'>$${products_in_cart[product].total.toFixed(2)}</span>
                 </h3>
             </div>
             <div style="display: flex; align-items: center;">
-                <img id="tru" style="border: 2px solid hsl(14, 25%, 72%); border-radius: 25px; height: 15px; padding: 1px;" onclick="removeFromCart('${product}')" src="./assets/images/icon-remove-item.svg"/>
+                <img style="border: 2px solid hsl(14, 25%, 72%); border-radius: 25px; height: 15px; padding: 1px;"
+                     onclick="removeFromCart('${product}')"
+                     src="./assets/images/icon-remove-item.svg"/>
             </div>
-        </div>
-        `;
-        document.getElementById('cart-total').innerText = total.toFixed(2);
+        </div>`;
     } else {
-        document.querySelector(`#${name}_quantity`).innerText = `${products_in_cart[product]["quantity"]}x`;
-        document.querySelector(`#${name}_total`).innerText = `$${products_in_cart[product]["total"].toFixed(2)}`;
-        document.getElementById('cart-total').innerText = total.toFixed(2);
+        document.querySelector(`#${name}_quantity`).innerText = `${products_in_cart[product].quantity}x`;
+        document.querySelector(`#${name}_total`).innerText = `$${products_in_cart[product].total.toFixed(2)}`;
     }
+}
 
+function updateCartSummary() {
+    const totalItems = Object.values(products_in_cart).reduce((sum, item) => sum + item.quantity, 0);
+    document.getElementById('total').innerText = totalItems;
+    document.getElementById('cart-total').innerText = total.toFixed(2);
 }
